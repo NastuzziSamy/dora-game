e986162730c3cb8bff6ded6f56fa7cb6c18bcf69
NastuzziSamy <samy@nastuzzi.fr>
2020-01-14 02:26:36 +0100

    Improve skill display again

--- src/screens/skill/interface.js
+++ src/screens/skill/interface.js
@@ -1,6 +1,6 @@
-const CANVAS_ATTACK = [0, 0, 360, 500];
-const CANVAS_POSSESSION = [360, 0, 360, 500];
-const CANVAS_DEFENSE = [720, 0, 360, 500];
+const CANVAS_ATTACK = [0, 0, 360, 800];
+const CANVAS_POSSESSION = [360, 0, 360, 800];
+const CANVAS_DEFENSE = [720, 0, 360, 800];
 const DIAMETER_SKILL_BUTTON = 40;
 
 const GAME_SCREEN_INTERFACE = {
@@ -24,10 +24,18 @@ const GAME_SCREEN_INTERFACE = {
 		textSize(16);
 		text("Augmente la létalité de l'infection", 180, 65);
 
+		if (mouseInRect(...CANVAS_ATTACK)) {
+			fill(...COLORS.WHITE_TRANSPARENT);
+			rect(...CANVAS_ATTACK);
+		}
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_ATTACK.attack_one.xp === BUTTON_ATTACK.attack_one.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_ATTACK.attack_one.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_ATTACK.attack_one.color);
+		fill(...COLORS.RED_ATTACK);
 		textAlign(CENTER, CENTER);
 		textFont("Arial");
 		textSize(22);
@@ -36,20 +44,19 @@ const GAME_SCREEN_INTERFACE = {
 			SKILL_SCREEN.drawInformationCompetence("Attaque I - " + BUTTON_ATTACK.attack_one.xp + "/" + BUTTON_ATTACK.attack_one.max_xp);
 		}
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_ATTACK.attack_two.xp === BUTTON_ATTACK.attack_two.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_ATTACK.attack_two.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_ATTACK.attack_two.color);
+		fill(...COLORS.RED_ATTACK);
 		textAlign(CENTER, CENTER);
 		textFont("Arial");
 		textSize(22);
 		text(['A'], ...BUTTON_ATTACK.attack_two.position);
 		if (mouseInRect(...BUTTON_ATTACK.attack_two.position, DIAMETER_SKILL_BUTTON, DIAMETER_SKILL_BUTTON, true)) {
 			SKILL_SCREEN.drawInformationCompetence("Attanquant II - " + BUTTON_ATTACK.attack_two.xp + "/" + BUTTON_ATTACK.attack_two.max_xp);
-		}
-
-		if (mouseInRect(...CANVAS_ATTACK)) {
-			fill(...COLORS.WHITE_TRANSPARENT);
-			rect(...CANVAS_ATTACK);
 		}
 	},
 
@@ -65,9 +72,18 @@ const GAME_SCREEN_INTERFACE = {
 		textSize(16);
 		text("Augmente la rapidité d'infection", 540, 65);
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (mouseInRect(...CANVAS_POSSESSION)) {
+			fill(...COLORS.WHITE_TRANSPARENT);
+			rect(...CANVAS_POSSESSION);
+		}
+
+		if (BUTTON_POSSESSION.possession_one.xp === BUTTON_POSSESSION.possession_one.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_POSSESSION.possession_one.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_POSSESSION.possession_one.color);
+		fill(...COLORS.BLUE_POSSESSION);
 		textAlign(CENTER, CENTER);
 		textFont("Arial");
 		textSize(22);
@@ -76,20 +92,19 @@ const GAME_SCREEN_INTERFACE = {
 			SKILL_SCREEN.drawInformationCompetence("Possession I - " + BUTTON_POSSESSION.possession_one.xp + "/" + BUTTON_POSSESSION.possession_one.max_xp);
 		}
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_POSSESSION.possession_two.xp === BUTTON_POSSESSION.possession_two.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_POSSESSION.possession_two.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_POSSESSION.possession_two.color);
+		fill(...COLORS.BLUE_POSSESSION);
 		textAlign(CENTER, CENTER);
 		textFont("Arial");
 		textSize(22);
 		text(['P'], ...BUTTON_POSSESSION.possession_two.position);
 		if (mouseInRect(...BUTTON_POSSESSION.possession_two.position, DIAMETER_SKILL_BUTTON, DIAMETER_SKILL_BUTTON, true)) {
 			SKILL_SCREEN.drawInformationCompetence("Possession II - " + BUTTON_POSSESSION.possession_two.xp + "/" + BUTTON_POSSESSION.possession_two.max_xp);
-		}
-
-		if (mouseInRect(...CANVAS_POSSESSION)) {
-			fill(...COLORS.WHITE_TRANSPARENT);
-			rect(...CANVAS_POSSESSION);
 		}
 	},
 
@@ -104,11 +119,19 @@ const GAME_SCREEN_INTERFACE = {
 		fill(200, 200, 200);
 		textSize(16);
 		text("Réduit la recherche et la résistance", 900, 65);
+		
+		if (mouseInRect(...CANVAS_DEFENSE)) {
+			fill(...COLORS.WHITE_TRANSPARENT);
+			rect(...CANVAS_DEFENSE);
+		}
 
-
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_DEFENSE.defense_one.xp === BUTTON_DEFENSE.defense_one.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_DEFENSE.defense_one.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_DEFENSE.defense_one.color);
+		fill(...COLORS.GREEN_SHIELD);
 		textAlign(CENTER, CENTER);
 		textFont("Arial");
 		textSize(22);
@@ -117,9 +140,13 @@ const GAME_SCREEN_INTERFACE = {
 			SKILL_SCREEN.drawInformationCompetence("Réduction résistance I - " + BUTTON_DEFENSE.defense_one.xp + "/" + BUTTON_DEFENSE.defense_one.max_xp);
 		}
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_DEFENSE.defense_two.xp === BUTTON_DEFENSE.defense_two.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_DEFENSE.defense_two.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_DEFENSE.defense_two.color);
+		fill(...COLORS.GREEN_SHIELD);
 		textAlign(CENTER, CENTER);
 		textSize(22);
 		text(['D'], ...BUTTON_DEFENSE.defense_two.position);
@@ -129,9 +156,13 @@ const GAME_SCREEN_INTERFACE = {
 
 
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_DEFENSE.resistance_one.xp === BUTTON_DEFENSE.resistance_one.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_DEFENSE.resistance_one.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_DEFENSE.resistance_one.color);
+		fill(...COLORS.GREEN_SHIELD_DARK);
 		textAlign(CENTER, CENTER);
 		textSize(22);
 		text(['D'], ...BUTTON_DEFENSE.resistance_one.position);
@@ -139,19 +170,18 @@ const GAME_SCREEN_INTERFACE = {
 			SKILL_SCREEN.drawInformationCompetence("Sabotage recherche I - " + BUTTON_DEFENSE.resistance_one.xp + "/" + BUTTON_DEFENSE.resistance_one.max_xp);
 		}
 
-		fill(...COLORS.WHITE_TRANSPARENT);
+		if (BUTTON_DEFENSE.resistance_two.xp === BUTTON_DEFENSE.resistance_two.max_xp) {
+			fill(...COLORS.WHITE);
+		} else {
+			fill(...COLORS.WHITE_TRANSPARENT);
+		}
 		circle(...BUTTON_DEFENSE.resistance_two.position, DIAMETER_SKILL_BUTTON);
-		fill(...BUTTON_DEFENSE.resistance_two.color);
+		fill(...COLORS.GREEN_SHIELD_DARK);
 		textAlign(CENTER, CENTER);
 		textSize(22);
 		text(['D'], ...BUTTON_DEFENSE.resistance_two.position);
 		if (mouseInRect(...BUTTON_DEFENSE.resistance_two.position, DIAMETER_SKILL_BUTTON, DIAMETER_SKILL_BUTTON, true)) {
 			SKILL_SCREEN.drawInformationCompetence("Sabotage recherche II - " + BUTTON_DEFENSE.resistance_two.xp + "/" + BUTTON_DEFENSE.resistance_two.max_xp);
-		}
-
-		if (mouseInRect(...CANVAS_DEFENSE)) {
-			fill(...COLORS.WHITE_TRANSPARENT);
-			rect(...CANVAS_DEFENSE);
 		}
 	},
 
